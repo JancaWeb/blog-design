2882078539e4d9ebaa8a49d9d09cab8b3c1361df
Kuba Janča <kubajanca@NB19080.local>
2019-09-06 23:49:36 +0200

    Updates

---
+++ precache-manifest.78d345e7b58bd61f7dc9bfb73e4ab60b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf06e2a7a96de26e160d2eccba29cb8",
+    "url": "/blog-design/index.html"
+  },
+  {
+    "revision": "e6f55f0d8687050c0360",
+    "url": "/blog-design/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fcc362d6126f475b884a",
+    "url": "/blog-design/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e6f55f0d8687050c0360",
+    "url": "/blog-design/static/js/2.2df48b09.chunk.js"
+  },
+  {
+    "revision": "fcc362d6126f475b884a",
+    "url": "/blog-design/static/js/main.11d07dbc.chunk.js"
+  },
+  {
+    "revision": "f048da2a31b6f292ec93",
+    "url": "/blog-design/static/js/runtime~main.1dd34277.js"
+  },
+  {
+    "revision": "a47e3deab0517b1bd80a25316784a182",
+    "url": "/blog-design/static/media/mainImage.a47e3dea.jpg"
+  },
+  {
+    "revision": "c927c434e0304275464cf137f5b47c14",
+    "url": "/blog-design/static/media/movie1.c927c434.mp4"
+  },
+  {
+    "revision": "c7b6e69480a9927a63cee5980ae890a9",
+    "url": "/blog-design/static/media/movie2.c7b6e694.mp4"
+  },
+  {
+    "revision": "3b849ab6c43f0aae803782466a0e362f",
+    "url": "/blog-design/static/media/movie3.3b849ab6.mp4"
+  },
+  {
+    "revision": "30887b2b97d9664a32a033a62ba699fa",
+    "url": "/blog-design/static/media/upperImage.30887b2b.jpeg"
+  }
+]);
--- src/App/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const GrowWrapper = styled.div`
-  transition: all 0.3s ease-out;
-  &:hover {
-    transform: scale(1.1);
-  }
-`;
